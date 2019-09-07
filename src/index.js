09e7074e8f74832d53a42c7df9c7f835bf6fa9f5
Skyler Riley <riley.skyler@gmail.com>
2019-09-06 20:42:15 -0700

    api url fix

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-console.log(API_URL);
-
 ReactDOM.render(
   <BrowserRouter>
     <CssBaseline />
