417318e5cadfbada30ec839d76f509bb09165523
Skyler Riley <riley.skyler@gmail.com>
2020-02-02 12:43:55 -0700

    documentation update

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,1 +1,10 @@
-console.log("typescript node");
+import Express from "express";
+
+const App = Express();
+const PORT = 1337 || process.argv[0];
+
+App.get("/", (req: Express.Request, res: Express.Response) =>
+  res.send("Hello World!")
+);
+
+App.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
